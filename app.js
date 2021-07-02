72aa31eab8207e4db1120f20ee5cf26fe7300c2e
sqc <570300991@qq.com>
2021-07-02 13:26:02 +0800

    update
    update

--- app.js
+++ app.js
@@ -12,27 +12,26 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        if (res.code) {
-          // 把获取到的code通过一个request的请求发给java服务器
-         wx.request({
-              url: config.local + '/mini/get/test',
-              data: {
-                code: res.code
-              },
-              method: 'GET',
-              dataType: 'json',
-              success: function (res) {
-                // 请求成功的处理
-                const data = res.data
-                if (data.code === '00000') {
-                  console.log('request Java res : ', data.data);
-                } else {
-                  console.log('error : ', data.msg);
-                }
-                
-              }
-          })
-        }
+        // if (res.code) {
+        //   // 把获取到的code通过一个request的请求发给java服务器
+        //  wx.request({
+        //       url: config.local + '/mini/get/test',
+        //       data: {
+        //         code: res.code
+        //       },
+        //       method: 'GET',
+        //       dataType: 'json',
+        //       success: function (res) {
+        //         // 请求成功的处理
+        //         const data = res.data
+        //         if (data.code === '00000') {
+        //           console.log('request Java res : ', data.data);
+        //         } else {
+        //           console.log('error : ', data.msg);
+        //         }
+        //       }
+        //   })
+        // }
       },
       fail: function () {
         console.log("request failed ：", res.data);  
